97df455e147bb6e3eba7ca95a38c2d0625a067e8
Bridget Spellicy <bspellicy@fingerlakes.edu>
2026-03-01 12:30:59 -0500

    Format menu options for temperature conversion

--- convert_C_to_F_02.py
+++ convert_C_to_F_02.py
@@ -17,8 +17,8 @@
 print("===== Temperature Converter =====")
 print()
 
-print("1. Convert from Celsius to Fahrenheit")
-print("2. Convert from Fahrenheit to Celsius")
+print("  1. Convert from Celsius to Fahrenheit")
+print("  2. Convert from Fahrenheit to Celsius")
 print()
 
 choice = input("Please choose from the above menu: ")
